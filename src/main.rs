1e6276505d9073ac2f06946ce40483a8a4b1ea3f
Wende Lu <wendelu1991@gmail.com>
2020-03-10 07:31:16 +0800

    try serde json

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
+
+extern crate serde_json;
+
+#[derive(Serialize, Deserialize)]
+pub struct TopLevel {
+    #[serde(rename = "name")]
+    name: String,
+
+    #[serde(rename = "method")]
+    method: String,
+
+    #[serde(rename = "protocol")]
+    protocol: String,
+
+    #[serde(rename = "host")]
+    host: String,
+
+    #[serde(rename = "path")]
+    path: String,
+
+    #[serde(rename = "query")]
+    query: Query,
+
+    #[serde(rename = "headers")]
+    headers: Headers,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Headers {
+    #[serde(rename = "Accept-Language")]
+    accept_language: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Query {
+    #[serde(rename = "foo")]
+    foo: i64,
+
+    #[serde(rename = "bar")]
+    bar: String,
+}
+
 fn main() {
     println!("Hello, world!");
 }
